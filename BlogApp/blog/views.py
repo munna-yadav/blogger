0707982456ee7d81ac540496a8acd80c30f8a86a
munna-yadav <mail.munna50@gmail.com>
2024-10-04 01:57:27 +0530

    feat: Add blog creating and editing

--- BlogApp/blog/views.py
+++ BlogApp/blog/views.py
@@ -1,17 +1,53 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from .models import Blog,Comment
+from django.contrib.auth.decorators import login_required
+from .forms import BlogForm
 # Create your views here.
 
 def home(request):
     blogs = Blog.objects.all().order_by('-created_at')
     return render(request,'blog/home.html',{"blogs":blogs})
-
+    
+@login_required
 def blog_detail(request, id):
     # Get the blog post by ID or return 404 if not found
     blog = get_object_or_404(Blog, id=id)
     
     # Pass the blog post to the template
     return render(request, 'blog/blog_detail.html', {'blog': blog})
+
+@login_required
+def create_blog(request):
+    if request.method == "POST":
+        form = BlogForm(request.POST, request.FILES)
+        if form.is_valid():
+            blog = form.save(commit=False)
+            blog.user = request.user
+            blog.save()
+            return redirect('home')
+    else:
+        form = BlogForm
+    return render (request,"blog/create_blog.html",{"form":form})
+
+
+@login_required
+def edit_blog(request,blog_id):
+    blog = get_object_or_404(Blog,pk=blog_id, user = request.user)
+    if request.method == 'POST':
+        form =  BlogForm(request.POST, request.FILES, instance=blog)
+        if form.is_valid():
+            form.save(commit=False)
+            blog.user = request.user
+            blog.save()
+            return redirect('home')
+    else:
+        form = BlogForm(instance=blog)
+    return render(request, 'create_blog.html',{"form":form})
+    
+        
+     
+    
+    
     
