70d91ed82b74ff9b10818ff540662d5b61c9889a
munna-yadav <mail.munna50@gmail.com>
2024-10-11 17:38:53 +0530

    commit: wholesale commit

--- BlogApp/blog/forms.py
+++ BlogApp/blog/forms.py
@@ -1,7 +1,12 @@
-from .models import Blog
+from .models import Blog, Comment
 from django import forms
 
 class BlogForm(forms.ModelForm):
     class Meta:
         model = Blog
         fields = ['title','theme','description','image']
+        
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['comment']
--- BlogApp/blog/urls.py
+++ BlogApp/blog/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path('',views.home,name = 'home'),
-    path('blog/<int:id>/',views.blog_detail,name='blog_deatil_view')
+    path("create/",views.create_blog, name="create_blog"),
+    path('<int:id>/',views.blog_detail,name='blog_detail_view'),
+    path('<int:blog_id>/edit',views.edit_blog, name="edit_blog"),
+    path('<int:blog_id>/delete',views.delete_blog, name="delete_blog"),
+    path('<int:blog_id>/comment', views.create_comment, name='create_comment'),
 ]
