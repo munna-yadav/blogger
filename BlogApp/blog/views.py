decf53d1e58966586112065f847040cbaf504a63
munna-yadav <mail.munna50@gmail.com>
2024-10-11 17:38:22 +0530

    feat: Add comment feature

--- BlogApp/blog/views.py
+++ BlogApp/blog/views.py
@@ -3,7 +3,9 @@ from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from .models import Blog,Comment
 from django.contrib.auth.decorators import login_required
-from .forms import BlogForm
+from .forms import BlogForm,CommentForm
+from django.views.generic import UpdateView
+from django.urls import reverse_lazy
 # Create your views here.
 
 def home(request):
@@ -22,13 +24,14 @@ def blog_detail(request, id):
 def create_blog(request):
     if request.method == "POST":
         form = BlogForm(request.POST, request.FILES)
+        print(request.FILES)
         if form.is_valid():
             blog = form.save(commit=False)
             blog.user = request.user
             blog.save()
             return redirect('home')
     else:
-        form = BlogForm
+        form = BlogForm()
     return render (request,"blog/create_blog.html",{"form":form})
 
 
@@ -42,12 +45,33 @@ def edit_blog(request,blog_id):
             blog.user = request.user
             blog.save()
             return redirect('home')
+        pass    
     else:
         form = BlogForm(instance=blog)
-    return render(request, 'create_blog.html',{"form":form})
-    
+    return render(request, 'blog/edit_blog.html',{"form":form})
+
+@login_required
+def delete_blog(request,blog_id):
+    blog = get_object_or_404(Blog,pk=blog_id, user = request.user)
+    if request.method == "POST":
+        blog.delete()
+        return redirect('home')
+    return render(request, 'blog/delete_dialog.html',{"blog":blog})
+
+@login_required
+def create_comment(request,blog_id):
+    blog = get_object_or_404(Blog,id=blog_id)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit= False)
+            comment.user = request.user
+            comment.blog = blog
+            comment.save()
+            return redirect('blog_detail',blog_id=blog.id)
         
-     
-    
+    else:
+        form = CommentForm()
+    return render(request,'blog/blog_detail.html',{'form':form,'blog':blog,'comment':comment})
     
     
